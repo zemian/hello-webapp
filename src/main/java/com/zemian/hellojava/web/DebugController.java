f43688cb21748ac91a06c0ad24da08ef55071b8f
Zemian Deng <zemiandeng@gmail.com>
2023-03-20 16:11:06 -0400

    Add debug controller

---
+++ src/main/java/com/zemian/hellojava/web/DebugController.java
@@ -0,0 +1,30 @@
+package com.zemian.hellojava.web;
+
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Just a sample of dummy Controller for debug code purpose.
+ */
+@WebServlet("/debug")
+public class DebugController extends HttpServlet {
+    private static final Logger LOG = getLogger(DebugController.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        AppManager app = AppManager.getInstance();
+        String name = req.getRequestURI();
+        LOG.debug("Using name={}", name);
+        String viewName = app.getViewResolver().resolveViewName("debug");
+        LOG.debug("Using viewName={}", name, viewName);
+        req.getRequestDispatcher(viewName).forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/debug.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<%@include file="/WEB-INF/jsp/includes/html-head.jsp"%>
+<%@include file="/WEB-INF/jsp/includes/header.jsp"%>
+
+<div class="container">
+    <div class="main-content">
+        <h1>Just a test</h1>
+        <p>Use this page to debug app code.</p>
+    </div>
+    <%@include file="/WEB-INF/jsp/includes/footer.jsp"%>
+</div>
+
+<%@include file="/WEB-INF/jsp/includes/html-tail.jsp"%>
+</html>
