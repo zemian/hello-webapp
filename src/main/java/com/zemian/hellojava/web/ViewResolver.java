63b918087daad1f0ae17aa1bb98297f966fc5938
Zemian Deng <zemiandeng@gmail.com>
2023-03-20 16:11:00 -0400

    Add debug controller

--- src/main/java/com/zemian/hellojava/web/ViewResolver.java
+++ src/main/java/com/zemian/hellojava/web/ViewResolver.java
@@ -1,13 +1,23 @@
 package com.zemian.hellojava.web;
 
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * Given a simple name and resolve the file location of a view file.
  */
 public class ViewResolver {
-    private String viewNamePrefix = "/WEB-INF/jsp/";
+    private static final Logger LOG = getLogger(ViewResolver.class);
+    private String viewNamePrefix = "/WEB-INF/jsp";
     private String viewNameSuffix = ".jsp";
 
     public String resolveViewName(String name) {
-        return viewNamePrefix + name + viewNameSuffix;
+        if (!name.startsWith("/")) {
+            name = "/" + name;
+        }
+        String view = viewNamePrefix + name + viewNameSuffix;
+        LOG.debug("Resolving view={}", view);
+        return view;
     }
 }
